12df0c63fb0735f4f869c14dab0bd2903308ac14
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-08 05:44:38 +0600

    default parameter add

---
+++ app.js
@@ -0,0 +1,12 @@
+// variable declare
+var marks = [12, 34, 65, 68, 90, 96, 42, 94];
+let fruit = ['mango', 'apple', 'jackfruit', 'watermilon'];
+const gadget = ['asus', 'hp', 'acer', 'samsung'];
+
+// default input 
+function add(num1, num2 = 0) {
+    sum = num1 + num2;
+    return sum;
+}
+const addTest = add(45);
+console.log(addTest);
