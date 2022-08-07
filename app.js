ad62c1951a1b3f104d68b19be5f6899746e5bd6d
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-08 05:48:54 +0600

    template string add

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ function add(num1, num2 = 0) {
     sum = num1 + num2;
     return sum;
 }
-const addTest = add(45);
+const addTest = add(45, 23);
 console.log(addTest);
+
+// template string 
+const age = 10;
+const name = 'Abdul Baten';
+const profile = `Hi, There is a ${name} and he is ${age} years old`;
+console.log(profile)
+
